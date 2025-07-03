77560b62f69e88a6b5bc9a32c86b3194f536c577
Varun Jp <104068367+Varunjp@users.noreply.github.com>
2025-07-03 10:28:54 +0530

    initial setup

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+	"retialops/routes"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error while loading env files")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		port = "3000"
+	}
+
+	router := gin.Default()
+	router.Use(gin.Logger(),gin.Recovery())
+
+	store := cookie.NewStore([]byte("secret-key"))
+	router.Use(sessions.Sessions("Mysession",store))
+
+	// add template helper
+
+	// load static files
+
+	// load html
+
+	routes.GetUrl(router)
+
+	router.Run(":"+port)
+
+}
